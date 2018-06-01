daa7ef8dcec20ea175454aefb93dd4e8338baf44
Alex Beregszaszi <alex@rtfs.hu>
2018-06-01 15:00:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ewasm-precompile-sha256
+
+Implementation of the SHA256 precompile in Rust.
+
+Can be compiled using:
+
+```
+cargo +nightly build --release --target wasm32-unknown-unknown
+```
+
+## Author(s)
+
+Alex Beregszaszi
+
+## License
+
+Apache 2.0
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+extern crate ewasm_api;
+extern crate sha2;
+use sha2::{Sha256, Digest};
+
+#[no_mangle]
+pub extern fn main() {
+  let length = ewasm_api::calldata_size();
+  let data = ewasm_api::calldata_copy(0, length);
+
+  // charge a base fee plus a word fee for every 256-bit word
+  let base_fee = 60;
+  let word_fee = 12;
+  let total_cost = base_fee + ((length + 31) / 32) * word_fee;
+
+  ewasm_api::consume_gas(total_cost as u64);
+
+  let mut hasher = Sha256::default();
+  hasher.input(&data);
+  let hash = hasher.result();
+
+  ewasm_api::finish_data(hash.to_vec());
+}
