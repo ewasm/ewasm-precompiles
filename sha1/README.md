df22fef24fa8aea64a43e6342b6c0999bf923153
Alex Beregszaszi <alex@rtfs.hu>
2018-12-21 21:49:08 +0000

    Add sha1 precompile

---
+++ sha1/README.md
@@ -0,0 +1,3 @@
+# sha1
+
+This is the precompile for enabling SHA-1 hashing according to [EIP-180](https://github.com/ethereum/EIPs/issues/180)
---
+++ sha1/src/lib.rs
@@ -0,0 +1,21 @@
+extern crate ewasm_api;
+extern crate sha1;
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn main() {
+    let length = ewasm_api::calldata_size();
+
+    // charge a base fee plus a word fee for every 256-bit word
+    let base_fee = 62;
+    let word_fee = 8;
+    let total_cost = base_fee + ((length + 31) / 32) * word_fee;
+
+    ewasm_api::consume_gas(total_cost as u64);
+
+    let data = ewasm_api::unsafe_calldata_copy(0, length);
+
+    let hash = sha1::Sha1::from(data).digest().bytes();
+
+    ewasm_api::finish_data(&hash)
+}
