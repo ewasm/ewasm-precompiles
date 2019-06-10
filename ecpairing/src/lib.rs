e2afd4f447ee0fdee1dca8148e13dd3ddc6949a4
Alexander Arlt <alexander.arlt@arlt-labs.com>
2019-06-10 15:22:20 -0400

    Remove usage of parity_bytes.

--- ecpairing/src/lib.rs
+++ ecpairing/src/lib.rs
@@ -1,12 +1,9 @@
 extern crate ethereum_bn128;
 extern crate ewasm_api;
-extern crate parity_bytes as bytes;
 
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn main() {
-    use bytes::BytesRef;
-
     let length = ewasm_api::calldata_size();
 
     // NOTE: this validation will also be done by bn128_pairing
@@ -25,7 +22,7 @@ pub extern "C" fn main() {
     let input = ewasm_api::calldata_acquire();
 
     let mut output = [0u8; 32];
-    match ethereum_bn128::bn128_pairing(&input[..], &mut BytesRef::Fixed(&mut output[..])) {
+    match ethereum_bn128::bn128_pairing(&input[..], &mut output) {
         Ok(_) => ewasm_api::finish_data(&output),
         Err(_) => ewasm_api::abort(),
     }
