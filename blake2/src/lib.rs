185c4261247b20a930e1a345cde96e0780c32b5b
Grant Wuerker <gwuerker@gmail.com>
2020-04-28 10:23:31 +0800

    Formatting.

--- blake2/src/lib.rs
+++ blake2/src/lib.rs
@@ -2,9 +2,9 @@ extern crate ewasm_api;
 
 mod f;
 
-use std::io::Cursor;
-use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
 use arr_macro::arr;
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::Cursor;
 
 #[cfg(not(test))]
 #[no_mangle]
@@ -14,7 +14,9 @@ pub extern "C" fn main() {
     }
 
     let mut data = Cursor::new(ewasm_api::calldata_acquire());
-    let rounds = data.read_u32::<BigEndian>().expect("Calldata size is invalid.");
+    let rounds = data
+        .read_u32::<BigEndian>()
+        .expect("Calldata size is invalid.");
     let mut h = arr![data.read_u64::<LittleEndian>().expect("Calldata size is invalid."); 8];
     let m = arr![data.read_u64::<LittleEndian>().expect("Calldata size is invalid."); 16];
     let t = arr![data.read_u64::<LittleEndian>().expect("Calldata size is invalid."); 2];
@@ -26,7 +28,9 @@ pub extern "C" fn main() {
 
     let mut h_bytes = vec![];
     for i in 0..8 {
-        h_bytes.write_u64::<LittleEndian>(h[i]).expect("Unable to write h.")
+        h_bytes
+            .write_u64::<LittleEndian>(h[i])
+            .expect("Unable to write h.")
     }
 
     ewasm_api::finish_data(h_bytes.as_slice())
